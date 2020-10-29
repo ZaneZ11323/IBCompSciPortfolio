fcb7bad4bb1b6a50531aade68772c92048ad9fb6
jander15 <45211598+jander15@users.noreply.github.com>
2020-10-29 15:02:55 -0600

    fixed And Gate

--- src/Project2/ElementaryLogicGates/And.java
+++ src/Project2/ElementaryLogicGates/And.java
@@ -3,11 +3,18 @@ package Project2.ElementaryLogicGates;
 public class And {
 
     public boolean out;
+    private Nand nand1;
+    private Not not1;
 
     public And() {
+        nand1 = new Nand();
+        not1 = new Not();
     }
 
     public void compute(boolean a, boolean b){
+        nand1.compute(a,b);
+        not1.compute(nand1.out);
+        out = not1.out;
 
     }
 }
