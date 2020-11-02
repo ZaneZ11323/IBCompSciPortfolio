1efbc49df9e97bb9c769ea268f2807d28edf4cd9
ZaneZ11323 <72717835+ZaneZ11323@users.noreply.github.com>
2020-11-02 14:56:56 -0700

    Done

--- src/Project2/ElementaryLogicGates/Mux.java
+++ src/Project2/ElementaryLogicGates/Mux.java
@@ -3,12 +3,13 @@ package Project2.ElementaryLogicGates;
 public class Mux {
 
     public boolean out;
+    Not not1 = new Not();
 
     public Mux() {
 
     }
 
     public void compute(boolean a, boolean b, boolean sel){
-
+    not1.compute(sel);
     }
 }
--- src/Project2/ElementaryLogicGates/Or.java
+++ src/Project2/ElementaryLogicGates/Or.java
@@ -3,13 +3,22 @@ package Project2.ElementaryLogicGates;
 public class Or {
 
     public boolean out;
+    private Nand nand1;
+    private Nand nand2;
+    private Nand nand3;
 
     public Or() {
+        nand1 = new And;
+        nand2= new And;
+        nand3 = new And;
 
     }
 
     public void compute(boolean a, boolean b){
-
+    nand1.compute(a);
+    nand2.compute(b);
+    nand3.compute(nand1.out, nand2.out);
+    out = nand3.out;
     }
 
 }
