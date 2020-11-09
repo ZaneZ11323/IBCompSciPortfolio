8ed25090975e13afaeff48b7cb238c7e09882bda
ZaneZ11323 <72717835+ZaneZ11323@users.noreply.github.com>
2020-11-09 14:38:06 -0700

    Done

--- src/Project2/ElementaryLogicGates/Mux.java
+++ src/Project2/ElementaryLogicGates/Mux.java
@@ -2,8 +2,12 @@ package Project2.ElementaryLogicGates;
 
 public class Mux {
 
+
     public boolean out;
     Not not1 = new Not();
+    And and1 = new And();
+    And and2 = new And();
+    Or  or1 = new Or();
 
     public Mux() {
 
@@ -11,5 +15,9 @@ public class Mux {
 
     public void compute(boolean a, boolean b, boolean sel){
     not1.compute(sel);
+    and1.compute(a,sel);
+    and2.compute(b, not1.out );
+    or1.compute(and1.out, and2.out);
+    or1.out=out;
     }
 }
