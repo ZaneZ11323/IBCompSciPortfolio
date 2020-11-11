303eb943f70b665fc8ef47b9deaf07437cc6f597
ZaneZ11323 <72717835+ZaneZ11323@users.noreply.github.com>
2020-11-11 11:22:30 -0700

    Mux16 works!

--- src/Project2/MultiBitVariants/Mux16.java
+++ src/Project2/MultiBitVariants/Mux16.java
@@ -1,15 +1,25 @@
 package Project2.MultiBitVariants;
 
+import Project2.ElementaryLogicGates.Mux;
+
 public class Mux16 {
     public boolean[] out = new boolean[16];
+    private Mux[] muxGates = new Mux[16];
 
 
     public Mux16() {
 
+        for (int i = 0; i < 16; i++) {
+            muxGates[i] = new Mux();
+        }
     }
 
     public void compute(boolean[] a,boolean[] b,boolean[] sel){
+        for (int i = 0; i < 16; i++) {
 
+            muxGates[i].compute(a[i],b[i],sel[i]);
+            out[i]=muxGates[i].out;
+        }
     }
 }
 
--- src/Project2/MultiBitVariants/Not16.java
+++ src/Project2/MultiBitVariants/Not16.java
@@ -2,6 +2,7 @@ package Project2.MultiBitVariants;
 
 
 public class Not16 {
+    public boolean[] out= new boolean[16];
 
 
     public Not16() {
