3f2eb0e0d2dc9dade9b97e8ca5ee4faa1e1862ad
ZaneZ11323 <72717835+ZaneZ11323@users.noreply.github.com>
2021-01-10 20:42:17 -0700

    Added my path names.

--- src/Project4/Main.java
+++ src/Project4/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         //File myFile = new File("/Users/ZZTOP/Desktop/COMSIPRJ4/demo.csv");
-        File myFile = new File("C:\\Users\\janderson\\Documents\\IB_Comp_Sci\\Student_Portfolios\\Zane\\IBCompSciPortfolio\\src\\Project4\\rawData.csv");
+        File myFile = new File("/Users/ZZTOP/Desktop/IBCOMPUTERSCIENCE /IBCompSciPortfolio/src/Project4/rawData.csv");
         Parser parser = new Parser(myFile);
         Double[] sortedArray = parser.sortAscending();
 
         //Writer writer = new Writer("/Users/ZZTOP/Desktop/COMSIPRJ4/output.csv");
-        Writer writer = new Writer("C:\\Users\\janderson\\Documents\\IB_Comp_Sci\\Student_Portfolios\\Zane\\IBCompSciPortfolio\\src\\Project4\\sorted.csv");
+        Writer writer = new Writer("/Users/ZZTOP/Desktop/IBCOMPUTERSCIENCE /IBCompSciPortfolio/src/Project4/sorted.csv");
         writer.writeArrayData(sortedArray);
     }
 }
