04500a4f71ca6a7a43840db0725e6399f57545e1
ZaneZ11323 <72717835+ZaneZ11323@users.noreply.github.com>
2021-03-22 18:00:06 -0600

    Completed assingment. No values for thursday, friday, saturday, and sunday

--- src/Project3/Main.java
+++ src/Project3/Main.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 public class Main {
 public static void main(String [] args){
 
-            JFrame window = new JFrame("Tic Tac Toe");
+            JFrame window = new JFrame("Skyber");
             window.setBounds(100, 100, 200, 400);
             window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             window.getContentPane().add(new Game());
